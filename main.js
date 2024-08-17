41fb76db096fede6ed406b81154149629a8be9b5
Antonio Piedra <anto.piedrapach@hotmail.com>
2024-08-17 13:32:00 -0600

    Add creation of grid

---
+++ main.js
@@ -0,0 +1,22 @@
+function createEtchRow(numberColumns) {
+  let etchRow = document.createElement("div");
+  etchRow.className = "etch-row";
+  for (let col = 0; col < numberColumns; col++) {
+    let pixel = document.createElement("div");
+    pixel.className = "etch-pixel";
+    etchRow.appendChild(pixel);
+  }
+  return etchRow;
+}
+
+function createGrid(numberRows, numberColumns) {
+  let etchContainer = document.querySelector(".etch-container");
+  let grid = [];
+  for (let row = 0; row < numberRows; row++) {
+    let etchRow = createEtchRow(numberColumns);
+    grid.push(etchRow);
+  }
+  etchContainer.replaceChildren(...grid);
+}
+
+createGrid(64, 64);
