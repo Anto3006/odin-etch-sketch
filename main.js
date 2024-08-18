4a9cb9c22b16973c8f8a0a4a77216d00ba49f2e0
Antonio Piedra <anto.piedrapach@hotmail.com>
2024-08-18 12:39:12 -0600

    Add need to have mouse down to sketch

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let mouseDown = false;
+
 function createEtchRow(numberColumns) {
   let etchRow = document.createElement("div");
   etchRow.className = "etch-row";
@@ -5,8 +7,9 @@ function createEtchRow(numberColumns) {
     let pixel = document.createElement("div");
     pixel.className = "etch-pixel";
     pixel.addEventListener("mouseover", (e) => {
-      console.log(e);
-      e.target.style.backgroundColor = "black";
+      if (mouseDown) {
+        e.target.style.backgroundColor = "black";
+      }
     });
     etchRow.appendChild(pixel);
   }
@@ -23,4 +26,18 @@ function createGrid(numberRows, numberColumns) {
   etchContainer.replaceChildren(...grid);
 }
 
+function checkMouseDown() {
+  let etchContainer = document.querySelector(".etch-container");
+  etchContainer.addEventListener("mousedown", () => {
+    mouseDown = true;
+  });
+  etchContainer.addEventListener("mouseup", () => {
+    mouseDown = false;
+  });
+  etchContainer.addEventListener("mouseleave", () => {
+    mouseDown = false;
+  });
+}
+
 createGrid(64, 64);
+checkMouseDown();
